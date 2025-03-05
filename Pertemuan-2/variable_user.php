b0712352e166ca1121b8b50dbfa71f4a717ee3c0
mohammad fiqri ramadhan <72728911+mohammadfiqri30@users.noreply.github.com>
2025-03-06 01:29:01 +0700

    Delete Pertemuan-2 directory

--- Pertemuan-2/variable_user.php
+++
@@ -1,9 +0,0 @@
-<?php
-// definisikan variables
-$nama = 'Mohammad Fiqri Ramadhan';
-$umur = 23;
-$berat = 50;
-echo "Nama saya adalah : " .$nama;
-echo '<br/>Umur : ' . $umur.' Tahun';
-echo '<br/>Berat : '.$berat.' Kg';
-?>
